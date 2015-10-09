538d61a04b9a04189b4c7e86d9b1e4b9185943e7
zezont4 <zezont@gmail.com>
2015-10-09 18:12:25 +0300

    add $myForm-> to menu

--- pages/nav_menu.php
+++ pages/nav_menu.php
@@ -3,9 +3,9 @@ $create_page = "
 <li class='dropdown'>
 	<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>" . $edited_table_name . "<span class='caret'></span></a>
 	<ul class='dropdown-menu' role='menu'>
-		<li class='{{ifActive('" . strtolower($edited_table_name) . ".index')}}'><a href='{{route('" . strtolower($edited_table_name) . ".index')}}'>" . $edited_table_name . "s</a></li>
-		<li class='{{ifActive('" . strtolower($edited_table_name) . ".create')}}'><a href='{{route('" . strtolower($edited_table_name) . ".create')}}'>جديد</a></li>
-		<li class='{{ifActive('" . strtolower($edited_table_name) . ".search')}}'><a href='{{route('" . strtolower($edited_table_name) . ".search')}}'>بحث</a></li>
+		<li class='{{\$myForm->ifActive('" . strtolower($edited_table_name) . ".index')}}'><a href='{{route('" . strtolower($edited_table_name) . ".index')}}'>" . $edited_table_name . "s</a></li>
+		<li class='{{\$myForm->ifActive('" . strtolower($edited_table_name) . ".create')}}'><a href='{{route('" . strtolower($edited_table_name) . ".create')}}'>جديد</a></li>
+		<li class='{{\$myForm->ifActive('" . strtolower($edited_table_name) . ".search')}}'><a href='{{route('" . strtolower($edited_table_name) . ".search')}}'>بحث</a></li>
 	</ul>
 </li>"; ?>
 <div class="clearfix col-xs-12">
